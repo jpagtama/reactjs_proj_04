9cc921dcc00449374d95fbce2244d7261360a6f4
Julian Pagtama <jpagtama@gmail.com>
2021-02-20 17:30:13 -0800

    Modified AppBar

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Slide from '@material-ui/core/Slide'
 
-export default function Header(props) {
+const HideOnScroll = (props) => {
+    const { children } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger();
+  
     return (
-        <AppBar>
-            <Toolbar>
-                Jules Development
-            </Toolbar>
-        </AppBar>
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
     )
 }
+
+const Header = (props) => {
+    return (
+        <React.Fragment >
+            <CssBaseline />
+            <HideOnScroll {...props}>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">Scroll to Hide App Bar</Typography>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar />
+        </React.Fragment>
+    )
+}
+
+export default Header
